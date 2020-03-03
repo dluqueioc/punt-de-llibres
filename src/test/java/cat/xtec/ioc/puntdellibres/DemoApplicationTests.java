4be408b3f862cb95b3e8d64f0af1def4cd2b7700
dluqueioc <david@ioc.cat>
2020-03-03 13:34:21 +0100

    Initial commit

---
+++ src/test/java/cat/xtec/ioc/puntdellibres/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.xtec.ioc.puntdellibres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
