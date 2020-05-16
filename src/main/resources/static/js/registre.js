c4323c3d17a96d2e2d68f174ea8991362a60f0de
David <david@ioc.cat>
2020-05-16 19:36:46 +0200

    Mostrar errors validació formulari registre

--- src/main/resources/static/js/registre.js
+++ src/main/resources/static/js/registre.js
@@ -1,8 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+	$('input').on('keydown', function () {
+		$(this).removeClass('invalid');
+	});
 
 	$("#form-submit-button").click(function(event) {
-		// if (!validar())
-			// return;
+		if (!validar()) return;
 
 		$("form").submit();
 	});
@@ -13,53 +16,71 @@ function validar() {
 }
 
 function isUsernameValid() {
-	var username = $('#username').val();
+	var $input = $('#username');
+	var helper = $('#helper-username')[0];
+	var username = $input.val();
 	if (username === "") {
-		alert("El camp Nom d'usuari no pot estar buit");
+		$input.addClass('invalid');
+		helper.dataset.error = "El nom d'usuari no pot estar buit";
 		return false;
 	}
 	if (username.length > 20 || username.length < 3) {
-		alert("El nom d'usuari ha de tenir entre 3 i 20 caràcters");
+		$input.addClass('invalid');
+		helper.dataset.error = "El nom d'usuari ha de tenir entre 3 i 20 caràcters";
 		return false;
 	}
 	return true;
 }
 
 function isEmailValid() {
-	var email = $('#email').val();
+	var $input = $('#email');
+	var helper = $('#helper-email')[0];
+	var email = $input.val();
 	if (email === "") {
-		alert("El camp email no pot estar buit");
+		$input.addClass('invalid');
+		helper.dataset.error = "L'email no pot estar buit";
 		return false;
 	}
 	if (email.length > 50) {
-		alert("L'email no pot superar els 50 caràcters");
+		$input.addClass('invalid');
+		helper.dataset.error = "L'email no pot superar els 50 caràcters";
 		return false;
 	}
 	return true;
 }
 
 function isPasswordValid() {
-	var password = $('#password').val();
+	var $input = $('#password');
+	var helper = $('#helper-password-1')[0];
+	var password = $input.val();
 	if (password === "") {
-		alert("Has d'escollir una contrasenya");
+		$input.addClass('invalid');
+		helper.dataset.error = "Has d'escollir una contrasenya";
 		return false;
 	}
 	if (password.length > 20 || password.length < 3) {
-		alert("La contrasenya ha de tenir entre 3 i 20 caràcters");
+		$input.addClass('invalid');
+		helper.dataset.error = "La contrasenya ha de tenir entre 3 i 20 caràcters";
 		return false;
 	}
 	return true;
 }
 
 function isPassword2Valid() {
-	var password1 = $('#password').val();
-	var password2 = $('#password2').val();
+	var $input1 = $('#password');
+	var $input2 = $('#password2');
+	var helper1 = $('#helper-password-1')[0];
+	var helper2 = $('#helper-password-2')[0];
+	var password1 = $input1.val();
+	var password2 = $input2.val();
 	if (password2 === "") {
-		alert("Has de repetir la contrasenya");
+		$input2.addClass('invalid');
+		helper2.dataset.error = "Has de repetir la contrasenya";
 		return false;
 	}
 	if (password2 !== password1) {
-		alert("Les contrasenyes no coincideixen");
+		$input2.addClass('invalid');
+		helper2.dataset.error = "Les contrasenyes no coincideixen";
 		return false;
 	}
 	return true;
